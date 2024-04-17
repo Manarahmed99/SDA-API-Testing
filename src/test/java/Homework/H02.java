9343e38ed42932bc346554bcd931c51cf9c02281
Manarahmed99 <manar6hmed@gmail.com>
2024-04-17 15:35:18 +0300

    Homework03 and 04

---
+++ src/test/java/Homework/H02.java
@@ -0,0 +1,55 @@
+package Homework;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+
+public class H02 {
+
+    /*
+       Given
+           https://reqres.in/api/users/2
+       When
+           User send GET Request to the URL
+       Then
+           HTTP Status Code should be 200
+       And
+           Response format should be "application/json"
+       And
+           "email" is "janet.weaver@reqres.in",
+       And
+           "first_name" is "Janet"
+       And
+           "last_name" is "Weaver"
+       And
+           "text" is "To keep ReqRes free, contributions towards server costs are appreciated!"
+    */
+
+    @Test
+    public void testUser2() {
+
+        String url = "https://reqres.in/api/users/2";
+        Response response = given().get(url);
+       // response.prettyPrint();
+
+        response
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data.email", equalTo("janet.weaver@reqres.in"))
+                .body("data.first_name", equalTo("Janet"))
+                .body("data.last_name", equalTo("Weaver"))
+                .body("support.text", equalTo("To keep ReqRes free, contributions towards server costs are appreciated!"));
+
+
+               
+
+
+
+    }
+}
