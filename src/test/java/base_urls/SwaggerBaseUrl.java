6f81e5dc4d4e7ea00a6ccda227c9bc79f4c914d7
Manarahmed99 <manar6hmed@gmail.com>
2024-04-25 12:13:14 +0300

    Homework week9

---
+++ src/test/java/base_urls/SwaggerBaseUrl.java
@@ -0,0 +1,22 @@
+package base_urls;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeMethod;
+
+public class SwaggerBaseUrl {
+
+
+
+
+    protected RequestSpecification spec;
+
+    @BeforeMethod
+    public void setUp() {
+        spec = new RequestSpecBuilder()
+                .setBaseUri("https://petstore.swagger.io/")
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+}
